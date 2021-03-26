bc67bbfb65db86c93a85cccde0d48ece60ad786f
Vanessa720 <vanessawilliamson718@gmail.com>
2021-03-26 15:02:21 -0400

    add contact me box

---
+++ script.js
@@ -0,0 +1,8 @@
+alert ("Welcome to my Portfolio!")
+
+const card = document.getElementsByClassName('box') [0]
+const cardTitle = card.getElementsByTagNameNS('h1') [0]
+const description = card.getElementsByTagName('p') [0]
+
+cardTitle.innerText = "Vanessa Williamson"
+description.innerText= "Vanessa Vanessa Vanessa"
