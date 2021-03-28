e7d040ff4f1177966366ec1104bc69135da3fe31
Vanessa720 <vanessawilliamson718@gmail.com>
2021-03-27 20:25:35 -0400

    finalizing finishing touches

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("button")
 
 button.addEventListener("click", function () {
-    alert("You'll be directed to e-mail! Thank you for chosing to contact me!")
+    alert("You'll be directed to e-mail! Thank you for choosing to contact me!")
 })
