a811c75da704af3e8b2e3914fe19c2722503b905
Vanessa720 <vanessawilliamson718@gmail.com>
2021-03-27 20:11:25 -0400

    Changed eventlistener message

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("button")
 
 button.addEventListener("click", function () {
-    alert("You've clicked on mail!")
+    alert("You'll be directed to e-mail! Thank you for chosing to contact me!")
 })
